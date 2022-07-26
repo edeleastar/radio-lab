67e082767711e9a891f077629c66d7574fcbe985
Eamonn de Leastar <edeleastar@wit.ie>
2022-07-26 06:47:07 +0100

    post messages to firebase

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import { createBot } from 'whatsapp-cloud-api';
 import dotenv from "dotenv";
-
+import { initializeApp } from "firebase/app";
+import { updateStr} from "./firebase-utils.js";
+import { child, get, getDatabase, ref, set,  push, runTransaction } from "firebase/database";
 const result = dotenv.config();
 if (result.error) {
   console.log(result.error.message);
   process.exit(1);
 }
+
+const firebaseConfig = {
+  apiKey: process.env.firebase_api_key,
+  databaseURL: process.env.firebase_database_url,
+  projectId: process.env.firebase_project_id,
+  appId: process.env.firebase_app_id
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+const db = getDatabase();
 
 (async () => {
   try {
@@ -21,6 +35,11 @@ if (result.error) {
 
     bot.on('message', async (msg) => {
       console.log(msg);
+      const msgListRef = ref(db, 'messages');
+      const newMessageRef = push(msgListRef);
+      set(newMessageRef, {
+        msg
+      });
     });
   } catch (err) {
     console.log(err);
